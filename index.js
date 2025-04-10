8b0722867e9f9c5491e192b4f47921224804e1bf
Julien Caussade <julien.caussade@gmail.com>
2025-04-10 12:14:15 +0200

    update

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Adishatz moùnde ! TEST");
+  res.send("Adishatz moùnde !");
 });
 
 app.listen("3001", () => {
