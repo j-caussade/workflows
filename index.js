4450f6cb985e05bbd63f5f440c95b79052951164
Julien Caussade <julien.caussade@gmail.com>
2025-04-10 12:11:23 +0200

    vercel ci test

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Adishatz moùnde !");
+  res.send("Adishatz moùnde ! TEST");
 });
 
 app.listen("3001", () => {
